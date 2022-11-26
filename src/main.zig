cb2551e32fd5bf00a4d1989ae3ceb18b31e33633
Daniel Bokser <dan.bokser@gmail.com>
2022-11-26 16:42:03 -0500

    fixed example code not drawing text

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,8 @@ pub export fn eventHandler(playdate: *pdapi.PlaydateAPI, event: pdapi.PDSystemEv
     switch (event) {
         .EventInit => {
             g_playdate_image = playdate.graphics.loadBitmap("playdate_image", null).?;
+            const font = playdate.graphics.loadFont("/System/Fonts/Asheville-Sans-14-Bold.pft", null).?;
+            playdate.graphics.setFont(font);
 
             playdate.system.setUpdateCallback(update_and_render, playdate);
         },
@@ -24,6 +26,7 @@ fn update_and_render(userdata: ?*anyopaque) callconv(.C) c_int {
     const playdate = @ptrCast(*pdapi.PlaydateAPI, @alignCast(@alignOf(pdapi.PlaydateAPI), userdata.?));
     const to_draw = "Hello from Zig!";
 
+    playdate.graphics.clear(@enumToInt(pdapi.LCDSolidColor.ColorWhite));
     const pixel_width = playdate.graphics.drawText(to_draw, to_draw.len, .UTF8Encoding, 0, 0);
     _ = pixel_width;
     playdate.graphics.drawBitmap(g_playdate_image, pdapi.LCD_COLUMNS / 2 - 16, pdapi.LCD_ROWS / 2 - 16, .BitmapUnflipped);
@@ -32,4 +35,3 @@ fn update_and_render(userdata: ?*anyopaque) callconv(.C) c_int {
     //we always want this frame drawn
     return 1;
 }
-
